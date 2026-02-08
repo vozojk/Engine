08c74cd4a199bfdf45c4d7dde403e9711b0d696e
vozojk <vojtech.sandera@gmail.com>
2026-02-08 17:34:40 +0100

    Implemented parsing for a system message

--- main.cpp
+++ main.cpp
@@ -1,16 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include "ITCH_Messages.hpp"
+
+// Put this at the top of your main.cpp or protocol.h
+
+#include <cstdint>
+
+// 1. Swap 16-bit (2 bytes) - used for Lengths and Message Types
+uint16_t bswap16(uint16_t x) {
+    return __builtin_bswap16(x);
+}
+
+// 2. Swap 32-bit (4 bytes) - used for Prices and Shares
+uint32_t bswap32(uint32_t x) {
+    return __builtin_bswap32(x);
+}
+
+// 3. Swap 64-bit (8 bytes) - used for Order IDs and Timestamps
+uint64_t bswap64(uint64_t x) {
+    return __builtin_bswap64(x);
+}
+
+//get timestamp into a 64 bit number
+uint64_t parseTimestamp(uint8_t* x) {
+    return ((uint64_t)x[0] << 40 |
+            (uint64_t)x[1] << 32 |
+            (uint64_t)x[2] << 24 |
+            (uint64_t)x[3] << 16 |
+            (uint64_t)x[4] << 8 |
+            (uint64_t)x[5]);
+}
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    //move the binary file to RAM (drive -> kernel cache(in RAM))
+    std::ifstream file("/home/vozojk/THE THING/market.bin", std::ios::binary);
+
+    //check if it actually loaded
+    if (!file) {
+        std::cerr << "FILE NOT READ SUCCESSFULLY" << std::endl;
+        return -1;
     }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    std::cout << "ENGINE STARTING!" << std::endl;
+
+    while (file) {
+
+        MessageHeader header;
+        file.read(reinterpret_cast<char*>(&header), sizeof(header)); //read the header part and create the object
+
+        if (file.gcount() == 0) break; //check if there was something
+
+        switch (header.MessageType) {
+            // switch for message type
+
+            case ('S'):{
+                SystemEvent event; //creates struct
+                event.MessageType = header.MessageType; //assigns type since it got eaten by the header
+                file.read(reinterpret_cast<char*>(&event.StockLocate), bswap16(header.Length) - 1);
+                //the whole length of each is sizeof(length)+sizeof(event), but the length variable only has sizeof(event)
+                //since we assigned the type manually (1 byte), we need to read length-1 bytes more
+
+
+                //endianness swaps
+                event.TrackingNumber = bswap16(event.TrackingNumber);
+                uint64_t timestamp = parseTimestamp(event.Timestamp);
+
+                //print for debug...
+                std::cout << "The type is " << event.MessageType << ", the locate is " << event.StockLocate
+                << ", the tracking number is " << event.TrackingNumber << ", the time was "
+                << timestamp << ", the event code is " << event.EventCode << "." << std::endl;
+
+                break;
+            }
+
+            default: {
+                //std::cout << "Unrecognized message type skipping..." << std::endl;
+
+                file.seekg(bswap16(header.Length) - 1, std::ios::cur);
+                //the whole length of each is sizeof(length)+sizeof(event), but the length variable only has sizeof(event)
+                //since we assigned the type manually (1 byte), we need to read length-1 bytes more
+                break;
+            }
+        }
+    }
+
 }
