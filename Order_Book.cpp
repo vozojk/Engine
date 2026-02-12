2af163da7735314d6e7e9a734c1123fae96d26e7
vozojk <vojtech.sandera@gmail.com>
2026-02-12 13:20:50 +0100

    Implemented executeOrder

--- Order_Book.cpp
+++ Order_Book.cpp
@@ -23,7 +23,7 @@ void addOrder(uint64_t Shares, uint64_t OrderID,
         Orders[OrderID] = it;
 
 }
-
+//cancels an order and updates the book at a certain price level accordingly, updates the rb tree(volume, delete from list) and hashmap (delete order)
 void cancelOrder(uint64_t OrderID) {
 
 
@@ -57,5 +57,49 @@ void cancelOrder(uint64_t OrderID) {
         }
 
         Orders.erase(hashIt);
+
+}
+//this is basically the same as cancel order
+//apparently the CPU keeps history of function calls and what happened after and makes predictions based on that
+//by separating them i make it more deterministic, the cpu will be right more often
+void executeOrder(uint64_t OrderID, uint64_t executedShares) {
+
+        auto hashIt = Orders.find(OrderID); //gives an iterator for the key value pair in the hashmap
+
+        if (hashIt == Orders.end()) return;
+
+        auto listIt = hashIt->second; //gives an iterator for the value in the hashmap at the iterator
+
+        PriceLevel *plList = listIt->Parent;
+        uint64_t &shares = listIt->Shares;
+        uint64_t &volume = plList->TotalVolume;
+
+        shares -= executedShares; //removes executedShares from the total shares of the order
+
+        char side = listIt->BuySell;
+        uint32_t Price = plList->Price;
+
+        volume -= executedShares;
+
+        //remove order if shares = 0, made a mistake, needs to be deleted first
+        //by deleting the PriceLevel i delete the orders list too
+        if (shares == 0) {
+
+                plList->orders.erase(listIt); //remove Order from the list
+                Orders.erase(hashIt); //remove iterator from the hashmap
+        }
+        //nesting this would be smart since i check for the volume only if shares are 0
+        if (volume == 0) {
+                if (side == 'S') {
+                        Asks.erase(Price);
+                }else {
+                        Bids.erase(Price);
+                }
+        }
+
+
+
+
+
 
 }
