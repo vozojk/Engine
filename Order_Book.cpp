9fbc22de04b303e83d2f14ed485032bfdb28fe6a
vozojk <vojtech.sandera@gmail.com>
2026-02-12 11:38:31 +0100

    Implemented AddOrder

--- Order_Book.cpp
+++ Order_Book.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Order_Book.hpp"
+#include "Types.hpp"
+
+std::map<uint32_t, PriceLevel, std::greater<uint32_t>> Bids;
+std::map<uint32_t, PriceLevel> Asks;
+std::unordered_map<uint64_t, std::list<Order>::iterator> Orders;
+
+void addOrder(uint64_t Shares, uint64_t OrderID,
+        uint16_t StockLocate, char BuySell, uint32_t Price) {
+
+        PriceLevel &level = (BuySell == 'B') ? Bids[Price] : Asks[Price];
+        //to the map
+        level.Price=Price;
+        level.TotalVolume += Shares;
+        level.orders.emplace_back(Shares,OrderID,&level,StockLocate,BuySell);
+
+        //to the hashmap
+        auto it = std::prev(level.orders.end());
+        Orders[OrderID] = it;
+
+}
