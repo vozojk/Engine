24a4d520a7ca9f1241f7d548f01d7949c81ae80c
vozojk <vojtech.sandera@gmail.com>
2026-02-12 11:38:15 +0100

    Added empty constructor for PriceLevel and a normal one for Order

--- Types.hpp
+++ Types.hpp
@@ -15,13 +15,21 @@ struct Order { //ordered to not waste space since theres padding
     PriceLevel* Parent;
     uint16_t StockLocate;
     char BuySell;
+
+    Order(uint64_t pShares, uint64_t pOrderID, PriceLevel* pParent,
+            uint16_t pStockLocate, char pBuySell) : Shares(pShares), OrderID(pOrderID), Parent(pParent), StockLocate(pStockLocate), BuySell(pBuySell) {
+    }
 };
 
 struct PriceLevel {
     uint64_t TotalVolume;
-    std::list<Order*> orders;
+    std::list<Order> orders;
     uint32_t Price;
 
+    PriceLevel() {
+        TotalVolume = 0;
+        Price = 0;
+    }
 };
 
 
