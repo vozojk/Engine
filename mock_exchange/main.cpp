9fd4b677cb4fca77b14ec38f653dc8371d940edf
vozojk <vojtech.sandera@gmail.com>
2026-03-06 15:55:21 +0100

    Responding to orders with accepted in TCP. Multithreaded

--- mock_exchange/main.cpp
+++ mock_exchange/main.cpp
@@ -12,6 +12,9 @@
 #include <cstring>        // memory manipulation
 #include <cstdint>        // standard integer types
 #include <iostream>       // console logging
+#include <thread>         // multithreading
+
+#include "OUCH_Messages.hpp"
 using namespace std;
 auto time(auto start_time, auto end_time, int counter) {
 
@@ -32,6 +35,7 @@ auto time(auto start_time, auto end_time, int counter) {
 }
 
 void sendBundledData(const char* filename, int udp_sock, const struct sockaddr_in dest) {
+    sleep(10);
     int fd = open(filename, O_RDONLY); //get a file descriptor for the bin file
     if (fd < 0) {
         perror("File failed to open!");
@@ -76,13 +80,13 @@ void sendBundledData(const char* filename, int udp_sock, const struct sockaddr_i
         }
         current_offset += len; //advance pointer to the next message
         //cout << "sent" << len << "bytes";
-        usleep(50);
+        usleep(5);
         if (counter % 1000000 == 0) {
             auto end_time = std::chrono::high_resolution_clock::now();
             time(start_time, end_time, 1000000);
             start_time = std::chrono::high_resolution_clock::now(); //start clock
         }
-        if (counter % 50000000 == 0) {//stop
+        if (counter % 2000000000 == 0) {//stop
             break;
         }
     }
@@ -95,10 +99,75 @@ void sendBundledData(const char* filename, int udp_sock, const struct sockaddr_i
     //close things and return
     close(fd);
 }
+
+void tcpResponder(int fd) {
+
+    cout << "Waiting for connection \n";
+    //wait for the accept socket of the engine
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(addr);
+    int sock = accept(fd, (sockaddr*) &addr, &len);
+
+    if (sock < 0) {
+        perror("Accept failed");
+        return;
+    }
+
+    cout << "Engine connected \n";
+
+    char buffer[2048];
+    uint64_t exchange_order_id = 1;
+
+    while (true) {
+        ssize_t size = recv(sock, buffer, sizeof(buffer), 0);
+        if (size > 0) {
+            //check for enter order
+            if (buffer[0] == 'O') {
+
+                EnterOrder* incoming = reinterpret_cast<EnterOrder *>(buffer);
+
+                Accepted back;
+                back.Type = 'A';
+
+                auto now = std::chrono::high_resolution_clock::now().time_since_epoch();
+                back.Timestamp = htobe64(std::chrono::duration_cast<std::chrono::nanoseconds>(now).count());
+
+                memcpy(back.ClOrdID, incoming->ClOrdID, 14);
+                memcpy(back.Symbol, incoming->Symbol, 8);
+                back.UserRefNum = incoming->UserRefNum;
+                back.Price = incoming->Price;
+                back.Quantity = incoming->Quantity;
+                back.Side = incoming->Side;
+                back.TimeInForce = incoming->TimeInForce;
+                back.Display = incoming->Display;
+                back.Capacity = incoming->Capacity;
+                back.InterMarketSweepEligibility = incoming->InterMarketSweepEligibility;
+                back.CrossType = incoming->CrossType;
+
+                back.OrderState = 'L';
+                back.OrderID = htobe64(exchange_order_id);
+                exchange_order_id++;
+
+                send(sock, &back, sizeof(back), 0);
+
+                cout << "Sent ACK for UserRefNum: " << ntohl(back.UserRefNum) << "\n";
+
+            }
+        } else if (size == 0) {
+            cout << "engine disconnected\n";
+            break;
+        }
+    }
+
+
+    close(sock);
+
+}
+//inialize sockets and start threads for tcp and udp
 int main() {
 
 
-
+    //udp
     int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
     //create a socket IPv4, UPD, default (0)
     //disable checksum since it could help rid of loopback
@@ -107,14 +176,33 @@ int main() {
         perror("setsockopt SO_NO_CHECK failed");
     }
 
-    sockaddr_in dest_addr{};
-    dest_addr.sin_family = AF_INET;//IPv4 family
-    dest_addr.sin_port = htons(12345); //big endian port
-    dest_addr.sin_addr.s_addr = inet_addr("172.28.197.104"); //localhost ip address
+    sockaddr_in udp_addr{};
+    udp_addr.sin_family = AF_INET;//IPv4 family
+    udp_addr.sin_port = htons(12345); //big endian port
+    udp_addr.sin_addr.s_addr = inet_addr("172.28.197.104"); //localhost ip address
 
-    sendBundledData("/home/vozojk/THE THING/resources/market.bin", udp_sock, dest_addr);
+    std::thread udp(sendBundledData, "/home/vozojk/THE THING/resources/market.bin", udp_sock, udp_addr);
+    cout << "UDP thread started";
 
+    //tcp
+    int tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(tcp_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
+    sockaddr_in tcp_addr{};
+    tcp_addr.sin_family = AF_INET;
+    tcp_addr.sin_addr.s_addr = INADDR_ANY;
+    tcp_addr.sin_port = htons(9000);
+
+    bind(tcp_sock, (sockaddr*)&tcp_addr, sizeof(tcp_addr));
+    listen(tcp_sock, 3);
+
+    std::thread tcp(tcpResponder, tcp_sock);
+
+    tcp.join();
+    udp.join();
+
+    close(tcp_sock);
     close(udp_sock);
     return 0;
 
